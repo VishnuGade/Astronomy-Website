fc212e65de125c17556d0ad19afd0d9ba8126aa1
VishnuGade <58572549+VishnuGade@users.noreply.github.com>
2020-11-30 14:11:25 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  //apiKey: "",
+  apiKey: "AIzaSyBdIf190yo_-pZRA2BTtPfeB_sK424_qlA",
   authDomain: "astronomy-website-6bbcfc.firebaseapp.com",
   databaseURL: "https://astronomy-website-6bbcfc.firebaseio.com",
   projectId: "astronomy-website-6bbcfc",
