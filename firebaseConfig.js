4ddfa6bf90e8a5277b9b086b83c325710fe4a0db
VishnuGade <58572549+VishnuGade@users.noreply.github.com>
2020-11-30 14:04:33 -0500

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  //apiKey: "",
+  authDomain: "astronomy-website-6bbcfc.firebaseapp.com",
+  databaseURL: "https://astronomy-website-6bbcfc.firebaseio.com",
+  projectId: "astronomy-website-6bbcfc",
+  storageBucket: "astronomy-website-6bbcfc.appspot.com",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var storage = firebase.storage();
