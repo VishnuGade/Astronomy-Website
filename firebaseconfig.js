936182f0b69a7fdae11d3a77680b1f6aabf4abdc
VishnuGade <58572549+VishnuGade@users.noreply.github.com>
2021-01-11 17:34:14 -0500

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -2,11 +2,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
   apiKey: "AIzaSyBdIf190yo_-pZRA2BTtPfeB_sK424_qlA",
-  authDomain: "astronomy-website-6bbcfc.firebaseapp.com",
-  databaseURL: "https://astronomy-website-6bbcfc.firebaseio.com",
-  projectId: "astronomy-website-6bbcfc",
-  storageBucket: "astronomy-website-6bbcfc.appspot.com",
-  messagingSenderId: "id"
+  authDomain: "astronomy-website-6bbcf.firebaseapp.com",
+  databaseURL: "https://astronomy-website-6bbcf.firebaseio.com",
+  projectId: "astronomy-website-6bbcf",
+  storageBucket: "astronomy-website-6bbcf.appspot.com",
+  messagingSenderId: "372062911875",
+  appId: "1:372062911875:web:234240f1bd206c68640ab6",
+  measurementId: "G-XVLPST579K"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
